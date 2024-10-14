192165f19e2ca20e224f5a8bed4668379384bcc4
Charlie Campbell <chacha9669@hotmail.com>
2024-10-14 15:34:56 +1100

    Binary Search

--- FileHandler/FileHandler.cpp
+++ FileHandler/FileHandler.cpp
@@ -8,13 +8,7 @@ using std::string;
 using std::ofstream;
 using std::ifstream;
 
-struct Player
-{
-    char name[32] = "";
-    int score = 0;
-    time_t time = 0;
-};
-
+//The main UI that allows the player to choose different modes
 int SelectMode()
 {
     system("CLS");
@@ -34,6 +28,8 @@ int SelectMode()
         std::cin >> choiceInput;
 
         system("CLS");
+
+        //Checks that the input is valid i.e. 1-4
         if (choiceInput.compare("1") == 0 || choiceInput.compare("2") == 0 || choiceInput.compare("3") == 0 || choiceInput.compare("4") == 0)
         {
             return std::stoi(choiceInput);
@@ -41,19 +37,21 @@ int SelectMode()
         else
         {
             std::cout << "Sorry, that input wasn't recognised.\n\n";
-            choiceInput = "0";
+            choiceInput = "0"; //Resets to stop infinite console loops
         }
     }
 }
 
 void DataEntry()
 {
+    //Opens file for appending
     ofstream file("high.scores", std::ios::app | std::ios::binary);
     if (file.good())
     {
+        //Temp player for storing user input
         Player newPlayer;
 
-        std::cout << "Please enter the name of the player.\nSpaces are not allowed.\n";
+        std::cout << "Please enter the name of the player.\nSpaces are not allowed, and max 32 characters.\n";
         std::cout << "Name: ";
         std::cin >> newPlayer.name;
 
@@ -61,29 +59,38 @@ void DataEntry()
         std::cout << "Score: ";
         std::cin >> newPlayer.score;
 
+        //Sets time to current time
         newPlayer.time = time(0);
 
+        //Writes the new player at the end of the file
         file.write((char*)&newPlayer, sizeof(Player));
 
+        //Converts time to english
         char timeStr[26];
         ctime_s(timeStr, sizeof(timeStr), &newPlayer.time);
-        std::cout << "\nSuccessfully saved " << newPlayer.name << " with a score of " << newPlayer.score << " at " << timeStr;
 
-        std::cout << "\n";
+        //User feedback response
+        std::cout << "\nSuccessfully saved " << newPlayer.name << " with a score of " << newPlayer.score << " at " << timeStr << "\n";
 
         std::cin.clear(); //clear bad input flag
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //discard input
+
+        //Closes file
         file.close();
+
+        //Sorts file
         SortFile();
     }
     else 
     {
         std::cout << "Failed to open high.scores.\n";
     }
+    //Closes file, incase of failed .good state
     file.close();
     system("pause");
 }
 
+//Returns the amount of player records in a file
 int GetPlayerCount()
 {
     int playerCount = 0;
@@ -92,7 +99,7 @@ int GetPlayerCount()
     if (file.good())
     {
         Player newPlayer;
-        //checks how many players are stored;
+        //This loop will run for every Player in high.scores
         while (!file.eof() && file.peek() != EOF)
         {
             file.read((char*)&newPlayer, sizeof(Player));
@@ -110,8 +117,11 @@ void SortFile()
     file.open("high.scores", std::ios::in | std::ios::binary);
     if (file.good())
     {
+        //Creates an array of players, with its size based on the amount of players that exist in the file
         Player* playerArray = new Player[playerCount];
-        //don't need to check for EOF as we already know how large the file is from previous loop
+
+        //don't need to check for EOF as we already know how large the file is from playerCount
+        //reads each player, for how many players there are
         file.read((char*)playerArray, sizeof(Player) * playerCount);
 
         //bubble sort players by name
@@ -124,9 +134,7 @@ void SortFile()
                 //Condition is true if first string is 'bigger', i.e. "Bob" and "Annie"
                 if (value > 0)
                 {
-                    //Swaps valyes
-                    char temp[32];
-
+                    //Swaps values
                     Player tempPlayer;
                     tempPlayer = playerArray[j];
                     playerArray[j] = playerArray[j + 1];
@@ -136,12 +144,14 @@ void SortFile()
         }
         file.close();
 
+        //Stores new sorted player list, overwriting previous
         ofstream fileOut("high.scores", std::ios::out | std::ios::binary);
         if (fileOut.good())
         {
             fileOut.write((char*)playerArray, sizeof(Player) * playerCount);
         }
 
+        //Closes file, frees memory
         fileOut.close();
         delete[] playerArray;
     }
@@ -153,28 +163,63 @@ void Lookup()
     ifstream file("high.scores", std::ios::in | std::ios::binary);
     if (file.good())
     {
+        //Gets and stores name for looking up
         char nameInput[32];
 
-        std::cout << "Please enter the name of the player.\nSpaces are not allowed.\n";
+        std::cout << "Please enter the name of the player.\nSpaces are not allowed, and max 32 characters.\n";
         std::cout << "Name: ";
         std::cin >> nameInput;
 
-        Player newPlayer;
+        //Binary search vars
+        int length = GetPlayerCount();
+        int beg = 0;
+        int end = length - 1;
+        int result = -1;
 
-        while (!file.eof() && file.peek() != EOF)
+        //Creates an array of players, with its size based on the amount of players that exist in the file
+        Player* playerArray = new Player[length];
+        //reads each player, for how many players there are
+        file.read((char*)playerArray, sizeof(Player) * length);
+
+        //Binary search algo
+        while (beg <= end)
         {
-            file.read((char*)&newPlayer, sizeof(Player));
-
-            if (strcmp(nameInput, newPlayer.name) == 0)
+            int mid = (beg + end) / 2;
+            //Checks if input comes after mid position
+            if (strcmp(playerArray[mid].name, nameInput) <= 0)
             {
-                std::cout << newPlayer.name << std::endl;
-                std::cout << newPlayer.score << std::endl;
-                char timeStr[26];
-                ctime_s(timeStr, sizeof(timeStr), &newPlayer.time);
-                std::cout << timeStr << std::endl << std::endl;
+                beg = mid + 1;
+                result = mid;
+            }
+            else
+            {
+                end = mid - 1;
             }
         }
+
+        //checks that the binary search result is actually equal
+        if (strcmp(playerArray[result].name, nameInput) != 0)
+        {
+            std::cout << "Sorry, that player was unable to be found." << std::endl;
+        }
+        else 
+        {
+            //Stores player based on result
+            Player newPlayer;
+            newPlayer = playerArray[result];
+
+            //displays results
+            std::cout << "Score: " << newPlayer.score << std::endl;
+            char timeStr[26];
+            ctime_s(timeStr, sizeof(timeStr), &newPlayer.time);
+            std::cout << "Time: " << timeStr << std::endl << std::endl;
+        }
+        
+        //frees memory
+        delete[] playerArray;
+
     }
+
     file.close();
     system("pause");
 
@@ -188,19 +233,26 @@ void Reading()
     if (file.good())
     {
         Player* playerArray = new Player[playerCount];
-        //don't need to check for EOF as we already know how large the file is from previous loop
+        //don't need to check for EOF as we already know how large the file is from playerCount
         file.read((char*)playerArray, sizeof(Player) * playerCount);
 
+        //Prints each player to the console
         for (int i = 0; i < playerCount; i++)
         {
-            std::cout << playerArray[i].name << std::endl;
-            std::cout << playerArray[i].score << std::endl;
+            std::cout << "Name: " << playerArray[i].name << std::endl;
+            std::cout << "Score: " << playerArray[i].score << std::endl;
+            //Converts from time_t to english
             char timeStr[26];
             ctime_s(timeStr, sizeof(timeStr), &playerArray[i].time);
-            std::cout << timeStr;
+            std::cout << "Time: " << timeStr;
             std::cout << "\n\n";
         }
+        //frees memory
         delete[] playerArray;
+    }
+    else
+    {
+        std::cout << "Failed opening file. Perhaps it doesn't exist?" << std::endl;
     }
     file.close();
     system("pause");
@@ -208,6 +260,7 @@ void Reading()
 
 int main()
 {
+    //Runs until broken by choice 4 (close)
     while (true)
     {
         int choice = SelectMode();
@@ -220,7 +273,7 @@ int main()
         }
         else if (choice == 2)
         {
-            std::cout << "You have selected lookup mode.\n";
+            std::cout << "You have selected lookup mode.\n\n";
             Lookup();
         }
         else if (choice == 3)
--- FileHandler/FileHandler.h
+++ FileHandler/FileHandler.h
@@ -1,5 +1,13 @@
 #pragma once
 
+struct Player
+{
+    char name[32] = "";
+    int score = 0;
+    time_t time = 0;
+};
+
+
 int SelectMode();
 void DataEntry();
 int GetPlayerCount();
